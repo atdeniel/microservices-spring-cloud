daccc356b9d91cf4f13d9b18e7776b4f180b9110
deniel <jdanielm001@gmail.com>
2024-02-18 16:58:14 -0300

    [UPDATE] Change to make it work

--- service-shopping/src/main/java/atdeniel/serviceshopping/service/InvoiceServiceImpl.java
+++ service-shopping/src/main/java/atdeniel/serviceshopping/service/InvoiceServiceImpl.java
@@ -25,11 +25,7 @@ public class InvoiceServiceImpl implements InvoiceService {
 
     @Autowired
     InvoiceItemsRepository invoiceItemsRepository;
-    @Autowired
-    CustomerClient customerClient;
 
-    @Autowired
-    ProductClient productClient;
 
     @Override
     public List<Invoice> findInvoiceAll() {
@@ -45,9 +41,6 @@ public class InvoiceServiceImpl implements InvoiceService {
         }
         invoice.setState("CREATED");
         invoiceDB = invoiceRepository.save(invoice);
-        invoiceDB.getItems().forEach( invoiceItem -> {
-            productClient.updateStockProduct( invoiceItem.getProductId(), invoiceItem.getQuantity() * -1);
-        });
 
         return invoiceDB;
     }
@@ -82,16 +75,6 @@ public class InvoiceServiceImpl implements InvoiceService {
     public Invoice getInvoice(Long id) {
 
         Invoice invoice= invoiceRepository.findById(id).orElse(null);
-        if (null != invoice ){
-            Customer customer = customerClient.getCustomer(invoice.getCustomerId()).getBody();
-            invoice.setCustomer(customer);
-            List<InvoiceItem> listItem=invoice.getItems().stream().map(invoiceItem -> {
-                Product product = productClient.getProduct(invoiceItem.getProductId()).getBody();
-                invoiceItem.setProduct(product);
-                return invoiceItem;
-            }).collect(Collectors.toList());
-            invoice.setItems(listItem);
-        }
         return invoice ;
     }
 }
